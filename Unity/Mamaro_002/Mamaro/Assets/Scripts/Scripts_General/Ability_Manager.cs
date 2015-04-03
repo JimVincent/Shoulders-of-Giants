fd509a2a2d85d5166261b179641a3b610251894f
JimVincent <1000343@student.sae.edu.au>
2015-04-03 16:19:24 +1000

    Worked on UI elements in Jim Test Scene

---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_General/Ability_Manager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ability_Manager : MonoBehaviour 
+{
+	// inspector assigned vars
+	public AbilitySocket[] sockets = new AbilitySocket[4];
+	public GameObject controlPanel_Parent;
+	public Text spareCoreDisplayText;
+
+	// private vars
+	private int spareCores = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+		// set all cores to deactive
+		SetSockets(0,0,0,0,0);
+
+		// set each ability sockets disabled and enabled pos
+		for(int i = 0; i < sockets.Length; ++i)
+		{
+			sockets[i].disabledPos = sockets[i].socketImage.transform.localPosition;
+		}
+	}
+
+	void Update()
+	{
+		// update spare core text
+		UpdateSparetext();
+	}
+
+
+	// sets each socket to the desired amount
+	public void SetSockets(int melee, int speed, int ranged, int shield, int spare)
+	{
+		sockets[0].SetActiveCores(melee);
+		sockets[1].SetActiveCores(speed);
+		sockets[2].SetActiveCores(ranged);
+		sockets[3].SetActiveCores(shield);
+		spareCores = spare;
+	}
+
+	// keeps spare core text updated
+	private void UpdateSparetext()
+	{
+		if(spareCores < 10)
+			spareCoreDisplayText.text = "0" + spareCores.ToString();
+		else
+			spareCoreDisplayText.text = spareCores.ToString();
+	}
+}
---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_Misc/AbilitySocket.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/*
+ * Represents a single ability socket which contains
+ * four cores that could be either active or deactive (represented by
+ * two sprite images)
+ * 
+ * Can change the states of the cores via public add and remove functions
+ * Contains getter func for active core count
+ * 
+*/
+
+[System.Serializable]
+public class AbilitySocket
+{
+	// inspector edited vars
+	public string abilityName;
+	public Image socketImage;
+	public Image[] coresImages = new Image[4];
+	public Sprite activeSprite, deactiveSprite;
+	
+	// inspector hidden vars
+	[HideInInspector]
+	public Vector3 disabledPos;
+	[HideInInspector]
+	public Vector3 enabledPos;
+
+	// private vars
+	private int activeCores = 0;
+
+	// changes the next deactive core to active if valid
+	public void AddCores()
+	{
+		// check capacity
+		if(activeCores < coresImages.Length)
+		{
+			activeCores++;
+			coresImages[activeCores-1].sprite = activeSprite;
+			// TODO Play foley add sound
+		}
+		else
+		{
+			// already at max capacity
+			// TODO Play foley full sound
+		}
+	}
+
+	// changes the last active core to deactive if valid
+	public void RemoveCores()
+	{
+		// check capacity
+		if(activeCores > 0)
+		{
+			activeCores--;
+			coresImages[activeCores-1].sprite = activeSprite;
+			// TODO Play foley remove sound
+		}
+		else
+		{
+			// already empty
+			// TODO Play foley empty sound
+		}
+	}
+
+	// sets scoket to the desired amount of active cores
+	public void SetActiveCores(int amount)
+	{
+		// make sure the amount is valid
+		if (amount > coresImages.Length)
+			Debug.LogError("Tried to assign " + amount + " cores to " + abilityName + " when max capacity is " + coresImages.Length);
+		else
+		{
+			// set active and deactive sprites
+			for(int i = 0; i < coresImages.Length; ++i)
+			{
+				if(i < amount)
+					coresImages[i].sprite = activeSprite;
+				else
+					coresImages[i].sprite = deactiveSprite;
+			}
+			
+			// ammend core count
+			activeCores = amount;
+		}
+	}
+
+	// returns the amount of active cores
+	public int GetCoreCount()
+	{
+		return activeCores;
+	}
+}
