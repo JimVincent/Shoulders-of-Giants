ab8b4c01f73e36ad3da742ad2852238899836eee
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-04-03 10:14:24 +1000

    Started Mamaro Movement

---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Charaters/Mamaro/MamaroMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MamaroMovement : MonoBehaviour {
+
+	public Vector3 moveDir;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//moveDir.x = Input.GetAxis ("Horizontal") * speed;
+		moveDir = transform.forward * Input.GetAxis ("Vertical") * speed;
+		moveDir.y = GetComponent<Rigidbody>().velocity.y;
+
+
+		transform.Rotate (new Vector3(0,Input.GetAxis ("Horizontal") * 2,0));
+
+		GetComponent<Rigidbody>().velocity = moveDir;
+	}
+}
