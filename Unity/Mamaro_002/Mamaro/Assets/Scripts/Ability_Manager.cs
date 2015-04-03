8a92fa8e1d37b77aa72f526f4200628bb000c008
JimVincent <1000343@student.sae.edu.au>
2015-04-03 12:16:50 +1000

    Added 'Jim Test' scene.
    Started On UI

---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Ability_Manager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ability_Manager : MonoBehaviour 
+{
+	public class AbilitySocket
+	{
+		public Image[] cores;
+
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
