477d3123ac7b840418971080202298f0c63ec178
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-04-05 15:24:03 +1000

    Added In Keyboard Controls

--- Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_Charaters/Mamaro/MamaroController.cs
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_Charaters/Mamaro/MamaroController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using XInputDotNetPure; // Required in C#
 
 public class MamaroController : MonoBehaviour {
+
+	public enum ControllerType{Keyboard, Contoller};
+
+	public ControllerType InputDevice;
 	//###########################################
 	//Required For X Input
 	bool playerIndexSet = false;
@@ -20,6 +24,19 @@ public class MamaroController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () 
+	{
+		switch (InputDevice)
+		{
+		case ControllerType.Contoller:
+			XboxController();
+			break;
+		case ControllerType.Keyboard:
+			KeyboardController();
+			break;
+		}
+	}
+
+	void XboxController()
 	{
 		//#############################################################
 		// Find a PlayerIndex, for a single player game
@@ -40,15 +57,15 @@ public class MamaroController : MonoBehaviour {
 		}
 		state = GamePad.GetState(playerIndex);
 		//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
+		
+		
 		//LeftStickMovement
 		move.moveDir = transform.right * state.ThumbSticks.Left.X / 2;
 		move.moveDir += transform.forward * state.ThumbSticks.Left.Y;
-
+		
 		//RightStickMovement
 		move.rotateEuler.y = state.ThumbSticks.Right.X;
-
+		
 		//LeftStick Clicked
 		//if moveing and not run for to long
 		if (move.moveDir.magnitude > 0 && move.timerRun < move.runMaxTime)
@@ -63,18 +80,17 @@ public class MamaroController : MonoBehaviour {
 		{
 			move.isRun = false;
 		}
-
+		
 		//Dodge Controls
 		if (state.Buttons.A == ButtonState.Pressed && prevState.Buttons.A == ButtonState.Released && move.moveDir.magnitude > 0)
 		{
 			Vector3 tempDir;
-			tempDir = transform.right * state.ThumbSticks.Left.X;
-			tempDir += transform.forward * state.ThumbSticks.Left.Y;
+			tempDir = move.moveDir;
 			tempDir = tempDir.normalized;
 			move.Dodge(tempDir);
 		}
-
-
+		
+		
 		if (state.DPad.Left == ButtonState.Pressed)
 		{
 			Ability_Manager_Chris_try.inst.SelectSocketLeft();
@@ -83,13 +99,53 @@ public class MamaroController : MonoBehaviour {
 		{
 			Ability_Manager_Chris_try.inst.SelectSocketRight();
 		}
-
-
-
-
 		//###################################
 		//Set Previous COntroller State
 		prevState = state;
 		//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	}
+
+	void KeyboardController()
+	{
+		//LeftStickMovement
+		move.moveDir = transform.right * Input.GetAxis("Horizontal") / 2;
+		move.moveDir += transform.forward * Input.GetAxis("Vertical");
+		
+		//RightStickMovement
+		move.rotateEuler.y = Input.GetAxis ("Mouse X");
+		
+		//LeftStick Clicked
+		//if moveing and not run for to long
+		if (move.moveDir.magnitude > 0 && move.timerRun < move.runMaxTime)
+		{
+			//LeftStick pressed and was not previously pressed
+			if (Input.GetKeyDown(KeyCode.LeftShift))
+			{
+				move.isRun = !move.isRun;
+			}
+		}
+		else
+		{
+			move.isRun = false;
+		}
+		
+		//Dodge Controls
+		if (Input.GetKeyDown(KeyCode.Space) && move.moveDir.magnitude > 0)
+		{
+			Vector3 tempDir;
+			tempDir = move.moveDir;
+			tempDir = tempDir.normalized;
+			move.Dodge(tempDir);
+		}
+		
+		
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Ability_Manager_Chris_try.inst.SelectSocketLeft();
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Ability_Manager_Chris_try.inst.SelectSocketRight();
+		}
 	}
 }
