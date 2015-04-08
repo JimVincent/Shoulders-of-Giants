dac157106cc012bc579a96072882418928f44095
CSchnitzerling <chris.schnitzerling@hotmail.com>
2015-04-08 10:28:55 +1000

    Started Lucy

---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_Charaters/Lucy/Lucy_Manager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lucy_Manager : MonoBehaviour {
+
+	Mamaro_Manager mamaro;
+
+	GameObject lucyTapping;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		mamaro = Mamaro_Manager.inst;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
