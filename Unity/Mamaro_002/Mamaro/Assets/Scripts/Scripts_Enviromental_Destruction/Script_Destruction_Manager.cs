30fd0638ac9ad0672d02bf72b8442e135155a41c
Eugene <jhonnywhiplash@hotmail.com.au>
2015-04-13 17:53:30 +1000

    Folder Merge Normal Maps Resized Destro Objects in City Working on

---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Scripts_Enviromental_Destruction/Script_Destruction_Manager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script_Destruction_Manager : MonoBehaviour {
+	public GameObject WallDestruction, buildDestrction;
+	public GameObject mamaro;
+	public BoxCollider mamaroFistCol;
+	public Script_Destruction_Chunks[] chunklets = new Script_Destruction_Chunks[0];
+	public int punchCount;
+	// Use this for initialization
+	void Awake () {
+		mamaro = GameObject.FindGameObjectWithTag ("Player");
+		mamaroFistCol = mamaro.GetComponentInChildren<BoxCollider> ();
+		if (gameObject.tag == "Wall") {
+			GameObject WallDestroLoad = Resources.Load ("Envi_Destructables/Kelpi_Wall_Busted") as GameObject;
+			WallDestruction = WallDestroLoad;
+		} else if (gameObject.tag == "Build") {
+			GameObject BuildDestroLoad = Resources.Load ("Envi_Destructables/L_Building_Model_Destro") as GameObject;
+			buildDestrction = BuildDestroLoad;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BlowUp(){
+		if (gameObject.tag == "Wall") {
+			GameObject.Destroy (this.gameObject);
+			Instantiate (WallDestruction, new Vector3 (transform.position.x, transform.position.y + 0, transform.position.z), transform.rotation);
+		} else if (gameObject.tag == "Build") {
+			GameObject.Destroy (this.gameObject);
+			Instantiate (buildDestrction, new Vector3 (transform.position.x, transform.position.y + 0, transform.position.z), transform.rotation);
+		}
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.collider == mamaroFistCol) {
+			Debug.Log ("MYFACE");
+			BlowUp();
+			punchCount ++;
+			Debug.Log(punchCount.ToString());
+		}
+
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+		if (col.collider == mamaroFistCol) {
+			Debug.Log ("Ouch");
+		}
+	}
+}
---
+++ Unity/Mamaro_002/Mamaro/Assets/Scripts/Tutorials/Tutorial_ShootPillar_HoldTriggers.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Tutorial_ShootPillar_HoldTriggers : MonoBehaviour {
+	public GameObject shootPillar;
+	public Tutorial_ShootPillar_AnimatorController shootPillarAnim;
+	public Script_Destruction_Manager[] childBlowUps = new Script_Destruction_Manager[0];
+	// Use this for initialization
+	void Start () {
+		shootPillar = GameObject.FindGameObjectWithTag ("Tutorial_ShootPillar");
+		shootPillarAnim = shootPillar.GetComponent<Tutorial_ShootPillar_AnimatorController> ();
+		childBlowUps = GetComponentsInChildren<Script_Destruction_Manager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1"))
+		{
+			TriggerFall();
+	}
+	}
+
+	////////////////////////////////////////////
+	/// NEED COLLISION CHECK
+	///////////////////////////////////////////
+	public void TriggerFall(){
+		for (int i = 0; i < childBlowUps.Length; i ++) {
+			childBlowUps [i].BlowUp ();
+		}
+
+		shootPillarAnim.Active();
+	}
+}
